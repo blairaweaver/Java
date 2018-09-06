892911bbb207aa4a34d62a96abd5be78e1231f53
blairaweaver <bw09135@georgiasouthern.edu>
2018-09-06 10:10:58 -0400

    Create Jewels.java
    Solution to Jewels and Stones Problem

---
+++ Jewels.java
@@ -0,0 +1,11 @@
+class Jewels {
+    public int numJewelsInStones(String J, String S) {
+        int sum = 0;
+        for (char c : S.toCharArray()){
+            if (J.indexOf(c) != -1){
+                sum++;
+            }
+        }
+        return sum;
+    }
+}
